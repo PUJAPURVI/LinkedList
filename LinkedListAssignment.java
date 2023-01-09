68ab0402ad031cfe08e2858aefe03a466539ad97
PUJAPURVI <nerkarpuja@gmail.com>
2023-01-09 11:41:09 +0530

    Added: Creation of Linked List With Values

--- LinkedListAssignment.java
+++ LinkedListAssignment.java
@@ -1,7 +1,24 @@
 package com.LinkedList;
 
+import java.util.LinkedList;
+
+/**
+ * @ author Puja
+ */
+
 public class LinkedListAssignment {
+
     public static void main(String[] args) {
+        /**
+         * Welcome message for user
+         * Method used for adding elements in LinkedList
+         */
         System.out.println("Welcome to Linked List Assignment");
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(56);
+        list.add(30);
+        list.add(70);
+        System.out.println(list);//uc1
     }
 }
+
